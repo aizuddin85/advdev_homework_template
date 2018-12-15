80b586cb7d2fedc1b9b976163654ead14cd516ec
aizuddin85 <mzali@redhat.com>
2018-12-15 18:09:48 +0800

    fixes on route

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -15,16 +15,23 @@ echo "Setting up Parks Production Environment in project ${GUID}-parks-prod"
 # To be Implemented by Student
 
 oc project ${GUID}-parks-prod
+
+# Give necessary RBAC access to ServiceAccounts.
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
 oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-parks-prod -n ${GUID}-parks-dev
 oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-dev
 
-
+# Create mongo services from template.
 oc create -f ../templates/mongodb-prod-svc.yaml
+# Create mongo StatefulSets from template.
 oc create -f ../templates/mongodb-prod-sts.yaml
+
+# Create Backend service from common backend template. This will hold as placeholder for Pipeline.
 oc process -f ../templates/backend-templates-prod.yaml  --param-file=params_file/mlbparks-green.params | oc create  -f -
 oc process -f ../templates/backend-templates-prod.yaml  --param-file=params_file/mlbparks-blue.params | oc create  -f -
 oc process -f ../templates/backend-templates-prod.yaml  --param-file=params_file/nationalparks-green.params | oc create  -f -
 oc process -f ../templates/backend-templates-prod.yaml  --param-file=params_file/nationalparks-blue.params | oc create  -f -
+
+# Create Frontend service from template. This will hold as placeholder for Pipeline.
 oc process -f ../templates/frontend-templates-prod.yaml --param-file=params_file/parksmap-green.params | oc create -f -
 oc process -f ../templates/frontend-templates-prod.yaml --param-file=params_file/parksmap-blue.params | oc create -f -
